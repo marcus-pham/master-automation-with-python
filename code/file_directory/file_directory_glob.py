14fa4cf1fa18af339fb74c3f334ab5a4e25db389
tanpham10 <phamtan500@gmail.com>
2019-07-24 14:06:27 +0700

    update

--- code/file_directory/file_directory_glob.py
+++ code/file_directory/file_directory_glob.py
@@ -1,72 +1,25 @@
 # scenario
-# create markdown toc
-# https://github.com/jonschlinkert/markdown-toc
-
 import glob
 import os
 import shutil
 
-# os.walk
-# https://stackoverflow.com/questions/10989005/do-i-understand-os-walk-right
-def understand_walk(root):
-	path = next(os.walk(root))[0]
-	print('path', path)
-
-	folders_in_path = next(os.walk(root))[1]
-	print('folders_in_path', folders_in_path)
-
-	files_in_path = next(os.walk(root))[2]
-	print('files_in_path', files_in_path)
-
-def understand_walk_with_for_loop(root):
-	for path, folders_in_path, files_in_path in os.walk(root):
-		print('path', path)
-		print('folders_in_path', folders_in_path)
-		print('files_in_path', files_in_path)
-		print('\n')
-
-def list_all_file_in_directory(root):
-	for path, folders_in_path, files_in_path in os.walk(root):
-		for file in files_in_path:
-			print(file)
-
-def copy_all_file_with_extension(source, destination, extension):
-	for path, folders_in_path, files_in_path in os.walk(source):
-		for file in files_in_path:
-			if file.endswith(extension):
-				print(file)
-				shutil.copy(os.path.join(path,file), destination)
-
-
-# root = r'C:\mawp\code\file_directory\root'
-# understand_walk(root)
-# understand_walk_with_for_loop(root)
-# list_all_file_in_directory(root)
-
-
-# source = r'C:\mawp\code\file_directory\root'
-# destination = r'C:\mawp\code\file_directory\destination'
-# copy_all_file_with_extension(source, destination, '.txt')
-
-
-
 # understand glob
-# https://pymotw.com/3/glob/
 root = r'C:\mawp\code\file_directory\root'
+
 def understand_glob(root):
-	# search all file with extension txt
+	# search all file under root only
 	print(glob.glob(os.path.join(root, '*.*')))
 
-	# search all file and directory inside root recursively
+	# search all file and directory under root recursively
 	print(glob.glob(os.path.join(root, '**'), recursive=True))
 
 
 understand_glob(root)
 
-# def copy_all_file_with_extension(source, destination, extension):
-# 	# copy file with extension from a source directory
-# 	files = glob.glob(os.path.join(source_dir, "*.md"))
-# 	for file in files:
-# 		if os.path.isfile(file):
-# 			print(file)
-# 			# shutil.copy2(file, dest_dir)
+
+def list_all_file_in_directory(root):
+	files_dirs = glob.glob(os.path.join(root, '**'), recursive=True)
+	print(files_dirs)
+
+	for item in files_dirs:
+		if os.path.isfile(item)
--- code/file_directory/file_directory_walk.py
+++ code/file_directory/file_directory_walk.py
@@ -38,35 +38,12 @@ def copy_all_file_with_extension(source, destination, extension):
 				shutil.copy(os.path.join(path,file), destination)
 
 
-# root = r'C:\mawp\code\file_directory\root'
-# understand_walk(root)
-# understand_walk_with_for_loop(root)
-# list_all_file_in_directory(root)
-
-
-# source = r'C:\mawp\code\file_directory\root'
-# destination = r'C:\mawp\code\file_directory\destination'
-# copy_all_file_with_extension(source, destination, '.txt')
-
-
-
-# understand glob
-# https://pymotw.com/3/glob/
 root = r'C:\mawp\code\file_directory\root'
-def understand_glob(root):
-	# search all file with extension txt
-	print(glob.glob(os.path.join(root, '*.*')))
-
-	# search all file and directory inside root recursively
-	print(glob.glob(os.path.join(root, '**'), recursive=True))
+understand_walk(root)
+understand_walk_with_for_loop(root)
+list_all_file_in_directory(root)
 
 
-understand_glob(root)
-
-# def copy_all_file_with_extension(source, destination, extension):
-# 	# copy file with extension from a source directory
-# 	files = glob.glob(os.path.join(source_dir, "*.md"))
-# 	for file in files:
-# 		if os.path.isfile(file):
-# 			print(file)
-# 			# shutil.copy2(file, dest_dir)
+source = r'C:\mawp\code\file_directory\root'
+destination = r'C:\mawp\code\file_directory\destination'
+copy_all_file_with_extension(source, destination, '.txt')
